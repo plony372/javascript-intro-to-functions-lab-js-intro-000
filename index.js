2a052472ab32e1f030e71e127b9dd2210ed90c09
plony benav <menachemfuterfas@gmail.com>
2021-01-24 18:00:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper (string){
 }
 
 function logShout (string){
-  console.log(string.toUpperCase(string))
+  console.log(string.toUpperCase())
 }
 
 function logwhisper (string){
-  console.log(string.toLowerCase(string))
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
