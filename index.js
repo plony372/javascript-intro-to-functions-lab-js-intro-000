a1a966cef31cd56aa4c09f8913f9117efebf9bda
plony benav <menachemfuterfas@gmail.com>
2021-01-24 17:55:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout (string){
+  return string.toUpperCase()
+}
+
+function whisper (string){
+  return string.toLowerCase()
+}
+
+function logShout (string){
+  console.log(string.toUpperCase(string))
+}
+
+function logwhisper (string){
+  console.log(string.toLowerCase(string))
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase(string) == "i love you, grandma."){
+    return "I love you, too."
+  } else if (string == uppercase) {
+    return "YES INDEED!"
+  }else if (string == lowercase){
+    return "I can't hear you!"
+  }
+}
+
